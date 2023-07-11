fb2516f3047633336a7c456ecd1da20f4fab3b91
F4ruk.seker <ahmetfaruk731@gmail.com>
2023-07-11 17:45:23 +0300

    switch product mode

--- config/settings/product.py
+++ config/settings/product.py
@@ -15,9 +15,9 @@ DATABASES = {
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['*','https://analiz.darken.gen.tr/','analiz.darken.gen.tr']
 
 
 CSRF_TRUSTED_ORIGINS = ['https://analiz.darken.gen.tr/', 'https://analiz.darken.gen.tr',]
