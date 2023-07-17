6101d09796f79b1ae1c104176b15a9c8025f666d
F4ruk.seker <ahmetfaruk731@gmail.com>
2023-07-17 15:41:34 +0300

    debug true

--- config/settings/product.py
+++ config/settings/product.py
@@ -15,7 +15,7 @@ DATABASES = {
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*','https://analiz.darken.gen.tr/','analiz.darken.gen.tr']
 
